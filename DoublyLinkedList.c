afb72ffb4567b532c1c41b7b648e5d3fd6727b4e
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-07 22:30:50 -0400

    Add struct node for linked list and insertLast()
    Struct node stores the value of the element and a pointer to the next
    and previous element in the doubly linked list. The insertLast()
    function takes in the head of the list and inserts an element at the end
    of the list.
    
    Adds utility to create a doubly linked list and insert an element at the
    end of the list.

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -1,9 +1,38 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+struct node
+{
+    int element;
+    struct node *next;
+    struct node *prev;
+};
+
+ struct node *insertLast(struct node *head, int newElement)
+ {
+     struct node *temp = head;
+     struct node *newNode = malloc(sizeof(struct node));
+     newNode->element = newElement;
+
+     if(head == NULL)
+     {
+         printf("The list is empty\n");
+         return;
+     }
+
+     while(temp->next != NULL)
+     {
+         temp = temp->next;
+     }
+     temp->next = newNode;
+     newNode->prev = temp;
+     return temp;
+
+ }
 
 int main()
 {
-
+    struct node *head = NULL;
+    head = insertLast(head, 10);
     return 0;
 }
