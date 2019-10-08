60f0eb0d28b67603bd36468919deda05353dff8d
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-07 22:54:35 -0400

    Add printList to print list from head or tail
    The printList(struct node *head) function takes in struct node pointer
    and prints the list in either normal order or reverse order dependent
    on if the pointer that is passed in is the head or the tail of the list.
    The function checks if the previous or next element is null to determine
    if the head or the tail of the list was passed in before printing.
    
    Access to a printList function allows the list to be printed for testing
    the functionality of the utility functions.

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -13,11 +13,12 @@ struct node
      struct node *temp = head;
      struct node *newNode = malloc(sizeof(struct node));
      newNode->element = newElement;
+     newNode->next = NULL;
 
      if(head == NULL)
      {
-         printf("The list is empty\n");
-         return;
+         head = newNode;
+         return head;
      }
 
      while(temp->next != NULL)
@@ -30,9 +31,56 @@ struct node
 
  }
 
+ void printList(struct node *head)
+ {
+     struct node *temp = head;
+
+     if(head == NULL)
+     {
+         printf("in else if");
+         printf("in if");
+         printf("The list is empty\n");
+         return;
+     }
+
+     //case when the input is the head of the list
+     else if(head->prev == NULL)
+     {
+
+         while(temp != NULL)
+         {
+             printf("%d ", temp->element);
+             temp = temp->next;
+         }
+         printf("\n");
+     }
+     //case when the input is the tail of the list
+
+     else if(head->next == NULL)
+     {
+
+         while(temp != NULL)
+         {
+             printf("%d ", temp->element);
+             temp = temp->prev;
+         }
+         printf("\n");
+     }
+
+     else
+        printf("Neither the head or the tail was passed into the function\n");
+
+ }
+
 int main()
 {
     struct node *head = NULL;
+
     head = insertLast(head, 10);
+    head->prev = NULL;
+    head = insertLast(head, 20);
+    printf("%d\n", head->element);
+
+    printList(head);
     return 0;
 }
